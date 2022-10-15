504f0e78de2cce298198bdb87f74c4d6276d647e
yigit <yigitozdamar@hotmail.com>
2022-10-15 18:42:14 +0300

    App Navigation Setup UITabBarController UINavigationController

---
+++ GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Yigit Ozdamar on 15.10.2022.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+}
