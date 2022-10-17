4f5ea3bd126fd328762c00d558a3f6609020e1f8
yigit <yigitozdamar@hotmail.com>
2022-10-17 19:16:17 +0300

    JSON Models Created

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Yigit Ozdamar on 17.10.2022.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GitHubFollowers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GitHubFollowers
+//
+//  Created by Yigit Ozdamar on 17.10.2022.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
