51a8af9dc45a630e81e8bc4cdb0e71b2720ad8a5
yigit <yigitozdamar@hotmail.com>
2022-10-18 15:25:04 +0300

    Refactor Error Messages

--- GitHubFollowers/Custom Views/GFButton.swift
+++ GitHubFollowers/Custom Views/GFButton.swift
@@ -27,7 +27,7 @@ class GFButton: UIButton {
     
     private func configure() {
         layer.cornerRadius = 10
-        titleLabel?.textColor = .white
+        setTitleColor(.white, for: .normal) //Normally we dont need because the default color is white.This is for example
         titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
         translatesAutoresizingMaskIntoConstraints = false
         
