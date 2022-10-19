d0ce699ff24c400ca91c903ce2009e255994f8dd
yigit <yigitozdamar@hotmail.com>
2022-10-19 20:30:47 +0300

    UICollectionView FlowLayout

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -27,7 +27,7 @@ class FollowerListVC: UIViewController {
     }
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlownLayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPink
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
@@ -36,6 +36,20 @@ class FollowerListVC: UIViewController {
     func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func createThreeColumnFlownLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding:CGFloat = 12
+        let minimumItemSpacing:CGFloat = 10
+        let avaibleWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = avaibleWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
     }
     
     func getFollowers() {
