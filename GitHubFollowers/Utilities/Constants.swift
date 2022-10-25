52b312be78317ea0aeee1525d5314204fa355b56
yigit <yigitozdamar@hotmail.com>
2022-10-25 15:14:05 +0300

    Item Info View

--- GitHubFollowers/Utilities/Constants.swift
+++ GitHubFollowers/Utilities/Constants.swift
@@ -9,4 +9,8 @@ import Foundation
 
 enum SFSymbols {
     static let location = "mappin.and.ellipse"
+    static let repos = "folder"
+    static let gitst = "text.alignleft"
+    static let followers = "heart"
+    static let following = "person.2"
 }
