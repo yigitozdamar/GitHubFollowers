9e1d6d767e305bc51fdf0b46907add3baafc8656
yigit <yigitozdamar@hotmail.com>
2022-10-26 14:30:10 +0300

    date conversion date label

---
+++ GitHubFollowers/Extensions/Date+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  Date+Ext.swift
+//  GitHubFollowers
+//
+//  Created by Yigit Ozdamar on 26.10.2022.
+//
+
+import Foundation
+
+extension Date {
+    
+    func convertToMonthYearFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"
+        
+        return dateFormatter.string(from: self)
+    }
+}
