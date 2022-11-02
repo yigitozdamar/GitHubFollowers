937ef9fcd92f728d51fd1d076e747837dedbfe51
yigit <yigitozdamar@hotmail.com>
2022-11-02 13:01:21 +0300

    Date() formatter bugs fixed

--- GitHubFollowers/Extensions/Date+Ext.swift
+++ GitHubFollowers/Extensions/Date+Ext.swift
@@ -9,10 +9,14 @@ import Foundation
 
 extension Date {
     
+//    func convertToMonthYearFormat() -> String {
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "MMM yyyy"
+//
+//        return dateFormatter.string(from: self)
+//    }
+    
     func convertToMonthYearFormat() -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMM yyyy"
-        
-        return dateFormatter.string(from: self)
+        return formatted(.dateTime.month().year())
     }
 }
